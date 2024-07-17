b00cbc0afe769a2951e4c9fae8a2785df099830c
David Höll <hoell@hl-dev.de>
2024-07-17 18:30:01 +0200

    feat: add example to ask llm

--- LlmUsability/LMStudioAgentManager.cs
+++ LlmUsability/LMStudioAgentManager.cs
@@ -17,6 +17,10 @@ namespace LlmUsability
             config = new LMStudioConfig("localhost", 1234);
         }
 
+        /// <summary>
+        /// Gets the default LMStudioAgent.
+        /// </summary>
+        /// <returns>The default LMStudioAgent.</returns>
         public LMStudioAgent GetDefaultLMStudioAgent()
         {
             return new LMStudioAgent(
@@ -25,13 +29,20 @@ namespace LlmUsability
                 temperature: 0);
         }
 
-        public LMStudioAgent GetEvolutionLMStudioAgent()
+        public LMStudioAgent GetEvolutionLMStudioAgent() 
+            => GetEvolutionLMStudioAgent("Gib nur eine JSON-Datei im Format: {\"Bewertung\": [Wert]} zurück. Keine Erklärungen oder zusätzlichen Informationen. Beurteile, wie gut die folgende Frage zur Antwort passt. Bewerte auf einer Skala von 1 bis 10000, wobei 1 schlecht und 10000 perfekt ist.");
+
+        /// <summary>
+        /// Gets the evolution LMStudioAgent. Set temperature to 0.
+        /// </summary>
+        /// <returns></returns>
+        public LMStudioAgent GetEvolutionLMStudioAgent(string systemMessage)
         {
             return new LMStudioAgent(
                 "asssistant",
                 config: config,
                 temperature: 0,
-                systemMessage: "Gib nur eine JSON-Datei im Format: {\"Bewertung\": [Wert]} zurück. Keine Erklärungen oder zusätzlichen Informationen. Beurteile, wie gut die folgende Frage zur Antwort passt. Bewerte auf einer Skala von 1 bis 10000, wobei 1 schlecht und 10000 perfekt ist.");
+                systemMessage: systemMessage);
         }
     }
 }
