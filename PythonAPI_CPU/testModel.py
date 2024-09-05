d6483dd96654526bb1e034bdc8205560465e8921
David Höll <hoell@hl-dev.de>
2024-09-05 22:19:57 +0200

    feat: how to install llama_cpp

--- PythonAPI_CPU/testModel.py
+++ PythonAPI_CPU/testModel.py
@@ -1,9 +1,11 @@
-import llama_cpp
+from llama_cpp import Llama
 
 # Load your GGUF model
-model = llama_cpp.Llama(model_path=r"D:\MyModels\Models\em_german_7b_v01.Q4_K_M.gguf")
+model = Llama(model_path=r"D:\MyModels\Models\em_german_7b_v01.Q4_K_M.gguf")
 
 # Example function to generate a response
 def generate_response(prompt):
     response = model(prompt)
     return response
+print("----------------------")
+print(generate_response("Wie geht es dir?"))
