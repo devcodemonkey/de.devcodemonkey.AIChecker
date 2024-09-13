68854867ec6a9aa34a2c497fba3fc7f16c124e5e
David Höll <hoell@hl-dev.de>
2024-09-13 15:51:25 +0200

    feat: Add in-memory database for unit testing
    Introduce in-memory database for `SendAPIRequestToLmStudioAndSaveToDbUseCaseTests` by adding `Microsoft.EntityFrameworkCore.InMemory` package. Update `DefaultMethodesRepository` to use in-memory `AicheckerContext` for isolated tests. Modify `UseCasesTests.csproj` to include the necessary package.

--- AIChecker/UseCasesTests/SendAPIRequestToLmStudioAndSaveToDbUseCaseTests.cs
+++ AIChecker/UseCasesTests/SendAPIRequestToLmStudioAndSaveToDbUseCaseTests.cs
@@ -7,19 +7,26 @@ using System.Text;
 using System.Threading.Tasks;
 using de.devcodemonkey.AIChecker.DataSource.APIRequester;
 using de.devcodemonkey.AIChecker.DataStore.SQLServerEF;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace de.devcodemonkey.AIChecker.UseCases.Tests
 {
     [TestClass()]
     public class SendAPIRequestToLmStudioAndSaveToDbUseCaseTests
     {
+        private readonly DbContextOptions<DbContext> _options = new DbContextOptionsBuilder<DbContext>()
+                .UseInMemoryDatabase(databaseName: "AicheckerTestDatabase")
+                .Options;
+
+
         [TestMethod()]
         public async Task ExecuteAsyncTest()
         {
             SendAPIRequestToLmStudioAndSaveToDbUseCase sendAPIRequestToLmStudioAndSaveToDbUseCase =
                 new SendAPIRequestToLmStudioAndSaveToDbUseCase(
                     new APIRequester(),
-                    new DefaultMethodesRepository());
+                    new DefaultMethodesRepository(new AicheckerContext(_options)));
 
             await sendAPIRequestToLmStudioAndSaveToDbUseCase.ExecuteAsync(
                 "How are you",
