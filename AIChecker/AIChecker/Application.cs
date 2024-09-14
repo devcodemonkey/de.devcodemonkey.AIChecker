6aa7432ed36606c7b485e3dc4dad26eeca14242e
David Höll <hoell@hl-dev.de>
2024-09-14 02:58:10 +0200

    feat: comment out hardcoded args array to use CLI arguments
    The hardcoded `args` array that was previously set to specific values has been commented out. This change allows the program to use the actual command-line arguments passed to it instead of the hardcoded ones, enhancing flexibility and usability.

--- AIChecker/AIChecker/Application.cs
+++ AIChecker/AIChecker/Application.cs
@@ -41,7 +41,7 @@ namespace de.devcodemonkey.AIChecker.AIChecker
 
         public async Task RunAsync(string[] args)
         {
-            args = ["sendToLMS", "-m", "Schreib mir ein Gedicht mit 100 Zeilen", "-s", "Du achtest darauf, dass sich alles reimt", "-r", "Requesttime check: | model: Phi-3.5-mini-instruct", "-c", "5", "-i", "5"];
+            //args = ["sendToLMS", "-m", "Schreib mir ein Gedicht mit 100 Zeilen", "-s", "Du achtest darauf, dass sich alles reimt", "-r", "Requesttime check: | model: Phi-3.5-mini-instruct", "-c", "5", "-i", "5"];
             //args = ["deleteResultSet", "-r", "cbc94e4a-868a-4751-aec1-9800dfbdcf08"];
             //args = ["viewResults", "-r", "7d26beed-3e04-4f7f-adb4-19bceca49503"];
             //args = ["view-used-gpu"];
