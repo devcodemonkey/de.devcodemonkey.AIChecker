c3b9349900f218d4fddb8e27e05a87cd37635acd
David Höll <hoell@hl-dev.de>
2024-09-20 21:45:24 +0200

    feat: add revision number and upload it to a server

--- AIChecker/CreateInnoInstaller.ps1
+++ AIChecker/CreateInnoInstaller.ps1
@@ -1,8 +1,10 @@
 function New-SetupFile {
     param (
         $version
-    )    
+    )        
     $innoPath = "C:\Program Files (x86)\Inno Setup 6"
+
+    $secureFtpPassword = Read-Host -AsSecureString "Enter ftp password"
 
     Set-Location $PSScriptRoot
     # change version in the vs project file
@@ -10,15 +12,91 @@ function New-SetupFile {
     [xml]$vsProjectFile = Get-Content .\AIChecker.csproj
     $vsProjectFile.Project.PropertyGroup.Version = $version
     $vsProjectFile.Save(".\AIChecker\AIChecker.csproj")
-    
+
     # change version in the installer script
-    Set-location ..
-    (Get-Content .\InstallerInno.iss) -replace "#define MyAppVersion ""##testing##""", "#define MyAppVersion ""$version""" | Set-Content .\InstallerInno.iss
+    Set-location ..           
+
+    $fileName = "Setup_$($version -replace '\.','_')"
+    (Get-Content .\InstallerInno.iss) -replace "#define MyAppVersion ""##testing##""", "#define MyAppVersion ""$version""" -replace "OutputBaseFilename=Setup", "OutputBaseFilename=$fileName" | Set-Content .\InstallerInno.iss
     
     # build the project
     Set-Location .\AIChecker\
     . dotnet publish -c Release -o .\bin\publish
     Set-Location ..
     ."$innoPath\ISCC.exe" InstallerInno.iss        
+
+    # create revision number
+    $revisionNumber = (Get-Date).ToString("yyyyMMddHHmmss")
+    Set-Location Output
+    New-Item -ItemType Directory -Path $revisionNumber    
+    Move-Item "$fileName.exe" $revisionNumber   
+
+
+
+
+    # Upload to FTP
+    # Script from https://gist.github.com/TheUltimateC0der/dd59fa051c3f797c5a79a38df2e1bdc1
+
+    # FTP Server Variables
+    $FTPHost = 'ftp://202.61.232.190/'
+    $FTPUser = 'aichecker'
+    $FTPPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureFtpPassword)
+    )
+  
+    #Directory where to find pictures to upload
+    $UploadFolder = (Get-Item -Path ".\").FullName    
+   
+    $webclient = New-Object System.Net.WebClient 
+    $webclient.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass)  
+  
+    $SrcEntries = Get-ChildItem $UploadFolder -Recurse
+    $Srcfolders = $SrcEntries | Where-Object { $_.PSIsContainer }
+    $SrcFiles = $SrcEntries | Where-Object { !$_.PSIsContainer }
+  
+    # Create FTP Directory/SubDirectory If Needed - Start
+    foreach ($folder in $Srcfolders) {    
+        $SrcFolderPath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"  
+        $DesFolder = $folder.Fullname -replace $SrcFolderPath, $FTPHost
+        $DesFolder = $DesFolder -replace "\\", "/"
+        # Write-Output $DesFolder
+  
+        try {
+            $makeDirectory = [System.Net.WebRequest]::Create($DesFolder);
+            $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
+            $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+            $makeDirectory.GetResponse();
+            #folder created successfully
+        }
+        catch [Net.WebException] {
+            try {
+                #if there was an error returned, check if folder already existed on server
+                $checkDirectory = [System.Net.WebRequest]::Create($DesFolder);
+                $checkDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
+                $checkDirectory.Method = [System.Net.WebRequestMethods+FTP]::PrintWorkingDirectory;
+                $response = $checkDirectory.GetResponse();
+                #folder already exists!
+            }
+            catch [Net.WebException] {
+                #if the folder didn't exist
+            }
+        }
+    }
+    # Create FTP Directory/SubDirectory If Needed - Stop
+  
+    # Upload Files - Start
+    foreach ($entry in $SrcFiles) {
+        $SrcFullname = $entry.fullname
+        $SrcName = $entry.Name
+        $SrcFilePath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"
+        $DesFile = $SrcFullname -replace $SrcFilePath, $FTPHost
+        $DesFile = $DesFile -replace "\\", "/"
+        # Write-Output $DesFile
+  
+        $uri = New-Object System.Uri($DesFile) 
+        $webclient.UploadFile($uri, $SrcFullname)
+    }
+    # Upload Files - Stop
 }
 
+New-SetupFile -version 0.0.3-alpha
