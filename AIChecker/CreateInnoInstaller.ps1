581021e986f43bdc7d15d576f2d9edf5324bff84
David Höll <hoell@hl-dev.de>
2024-09-21 01:56:38 +0200

    feat: add link to json and create html for deployment

--- AIChecker/CreateInnoInstaller.ps1
+++ AIChecker/CreateInnoInstaller.ps1
@@ -1,10 +1,14 @@
 function New-SetupFile {
     param (
-        $version
+        $version,
+        $upload = $false
+
     )        
     $innoPath = "C:\Program Files (x86)\Inno Setup 6"
-
-    $secureFtpPassword = Read-Host -AsSecureString "Enter ftp password"
+    if ($upload) {
+        $secureFtpPassword = Read-Host -AsSecureString "Enter ftp password"    
+    }
+    
 
     Set-Location $PSScriptRoot
     # change version in the vs project file
@@ -37,7 +41,7 @@ function New-SetupFile {
     $jsonContent = @()
 
     if (Test-Path $jsonFileName) {
-        $jsonContent = Get-Content -Path $jsonFileName | ConvertFrom-Json
+        $jsonContent = Get-Content -Path $jsonFileName | ConvertFrom-Json        
         if ($jsonContent -isnot [array]) {
             $jsonContent = @($jsonContent)  # Convert to an array if it's not already one
         }
@@ -47,78 +51,86 @@ function New-SetupFile {
         @{            
             revision = "$revisionNumber";
             file     = "$fileName.exe";            
-            version  = "$version";            
+            version  = "$version";                   
+            link     = "https://devcodemonkey.de/AiChecker/$revisionNumber/$fileName.exe"    
         }
-    )
-
-    $jsonContent += $jsonObject
+    )   
+    
+    $jsonContent += $jsonObject    
+    
     $jsonString = $jsonContent | ConvertTo-Json -Depth 3
+    if ($jsonString.Substring(0, 1) -ne '[') {
+        $jsonString = "[$jsonString]"        
+    }
     Set-Content -Path $jsonFileName -Value $jsonString
+
+    Copy-Item ..\index.html .\index.html
 
     # Upload to FTP
     # Script from https://gist.github.com/TheUltimateC0der/dd59fa051c3f797c5a79a38df2e1bdc1
-
-    # FTP Server Variables
-    $FTPHost = 'ftp://202.61.232.190/'
-    $FTPUser = 'aichecker'
-    $FTPPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureFtpPassword)
-    )
+    if ($upload) {
+        # FTP Server Variables    
+        $FTPHost = 'ftp://202.61.232.190/'
+        $FTPUser = 'aichecker'
+        $FTPPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+            [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureFtpPassword)
+        )
   
-    #Directory where to find pictures to upload
-    $UploadFolder = (Get-Item -Path ".\").FullName    
+        #Directory where to find pictures to upload
+        $UploadFolder = (Get-Item -Path ".\").FullName    
    
-    $webclient = New-Object System.Net.WebClient 
-    $webclient.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass)  
+        $webclient = New-Object System.Net.WebClient 
+        $webclient.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass)  
   
-    $SrcEntries = Get-ChildItem $UploadFolder -Recurse
-    $Srcfolders = $SrcEntries | Where-Object { $_.PSIsContainer }
-    $SrcFiles = $SrcEntries | Where-Object { !$_.PSIsContainer }
+        $SrcEntries = Get-ChildItem $UploadFolder -Recurse
+        $Srcfolders = $SrcEntries | Where-Object { $_.PSIsContainer }
+        $SrcFiles = $SrcEntries | Where-Object { !$_.PSIsContainer }
   
-    # Create FTP Directory/SubDirectory If Needed - Start
-    foreach ($folder in $Srcfolders) {    
-        $SrcFolderPath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"  
-        $DesFolder = $folder.Fullname -replace $SrcFolderPath, $FTPHost
-        $DesFolder = $DesFolder -replace "\\", "/"
-        # Write-Output $DesFolder
+        # Create FTP Directory/SubDirectory If Needed - Start
+        foreach ($folder in $Srcfolders) {    
+            $SrcFolderPath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"  
+            $DesFolder = $folder.Fullname -replace $SrcFolderPath, $FTPHost
+            $DesFolder = $DesFolder -replace "\\", "/"
+            # Write-Output $DesFolder
   
-        try {
-            $makeDirectory = [System.Net.WebRequest]::Create($DesFolder);
-            $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
-            $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
-            $makeDirectory.GetResponse();
-            #folder created successfully
-        }
-        catch [Net.WebException] {
             try {
-                #if there was an error returned, check if folder already existed on server
-                $checkDirectory = [System.Net.WebRequest]::Create($DesFolder);
-                $checkDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
-                $checkDirectory.Method = [System.Net.WebRequestMethods+FTP]::PrintWorkingDirectory;
-                $response = $checkDirectory.GetResponse();
-                #folder already exists!
+                $makeDirectory = [System.Net.WebRequest]::Create($DesFolder);
+                $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
+                $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+                $makeDirectory.GetResponse();
+                #folder created successfully
             }
             catch [Net.WebException] {
-                #if the folder didn't exist
+                try {
+                    #if there was an error returned, check if folder already existed on server
+                    $checkDirectory = [System.Net.WebRequest]::Create($DesFolder);
+                    $checkDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser, $FTPPass);
+                    $checkDirectory.Method = [System.Net.WebRequestMethods+FTP]::PrintWorkingDirectory;
+                    $response = $checkDirectory.GetResponse();
+                    #folder already exists!
+                }
+                catch [Net.WebException] {
+                    #if the folder didn't exist
+                }
             }
         }
-    }
-    # Create FTP Directory/SubDirectory If Needed - Stop
+        # Create FTP Directory/SubDirectory If Needed - Stop
   
-    # Upload Files - Start
-    foreach ($entry in $SrcFiles) {
-        $SrcFullname = $entry.fullname
-        $SrcName = $entry.Name
-        $SrcFilePath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"
-        $DesFile = $SrcFullname -replace $SrcFilePath, $FTPHost
-        $DesFile = $DesFile -replace "\\", "/"
-        # Write-Output $DesFile
+        # Upload Files - Start
+        foreach ($entry in $SrcFiles) {
+            $SrcFullname = $entry.fullname
+            $SrcName = $entry.Name
+            $SrcFilePath = $UploadFolder -replace "\\", "\\" -replace "\:", "\:"
+            $DesFile = $SrcFullname -replace $SrcFilePath, $FTPHost
+            $DesFile = $DesFile -replace "\\", "/"
+            # Write-Output $DesFile
   
-        $uri = New-Object System.Uri($DesFile) 
-        $webclient.UploadFile($uri, $SrcFullname)
+            $uri = New-Object System.Uri($DesFile) 
+            $webclient.UploadFile($uri, $SrcFullname)
+        }
     }
     # Upload Files - Stop
 }
 
-New-SetupFile -version 0.0.1-alpha
+New-SetupFile -version 0.0.1-alpha -upload $false
 git reset --hard
