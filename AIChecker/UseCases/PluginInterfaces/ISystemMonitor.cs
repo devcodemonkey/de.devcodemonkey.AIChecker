df38048985ca4fde01d3e2dc6f806f5785cad7c5
David Höll <hoell@hl-dev.de>
2024-09-30 11:12:50 +0200

    feat: add mew gpu usage methode to interface

--- AIChecker/UseCases/PluginInterfaces/ISystemMonitor.cs
+++ AIChecker/UseCases/PluginInterfaces/ISystemMonitor.cs
@@ -1,4 +1,5 @@
 ﻿using de.devcodemonkey.AIChecker.CoreBusiness.DbModels;
+using de.devcodemonkey.AIChecker.CoreBusiness.Models;
 using de.devcodemonkey.AIChecker.CoreBusiness.ModelsSystemMonitor;
 
 namespace de.devcodemonkey.AIChecker.UseCases.PluginInterfaces
@@ -6,7 +7,9 @@ namespace de.devcodemonkey.AIChecker.UseCases.PluginInterfaces
     public interface ISystemMonitor
     {
         Task<IEnumerable<SystemResourceUsage>> GetApplicationUsagesAsync();
+        List<GpuMemoryUsage> GetGpuMemoryUsages();
         Task<GpuStatData> GetGpuUsageAsync();
+        List<GpuProcessUsage> GetProcessUsage();
         Task MonitorPerformanceEveryXSecondsAsync(Action<IEnumerable<SystemResourceUsage>> saveAction, int intervalSeconds, CancellationToken cancellationToken, bool writeOutput = false);
     }
 }
