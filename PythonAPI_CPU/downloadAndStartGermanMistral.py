fbab273a8dcf5842b0ff487fdd26a3a233c2fa16
David Höll <hoell@hl-dev.de>
2024-10-05 11:41:59 +0200

    feat: transfer python script to other repository

--- PythonAPI_CPU/downloadAndStartGermanMistral.py
+++
@@ -1,6 +0,0 @@
-from ctransformers import AutoModelForCausalLM
-
-# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/em_german_13b_v01-GGUF", model_file="em_german_13b_v01.Q4_K_M.gguf", model_type="llama", gpu_layers=50)
-
-print(llm("AI is going to"))
