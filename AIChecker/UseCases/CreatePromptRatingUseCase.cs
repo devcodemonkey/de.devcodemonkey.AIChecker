54765b90dae84e5bc18c9e9f15a43f7c2f792f41
David Höll <hoell@hl-dev.de>
2024-10-14 21:38:05 +0200

    feat: CreatePromptRatingUseCase and add test for it
    - Updated `CreatePromptRatingUseCaseTests.cs` to use an in-memory database for testing, including changes to `ExecuteAsyncTest`.
    - Added `CreatePromptRatingUseCase.cs` with `ExecuteAsync` method, handling model loading, chat requests, and result processing.

---
+++ AIChecker/UseCases/CreatePromptRatingUseCase.cs
@@ -0,0 +1,85 @@
+﻿using de.devcodemonkey.AIChecker.CoreBusiness.DbModels;
+using de.devcodemonkey.AIChecker.CoreBusiness.Interfaces;
+using de.devcodemonkey.AIChecker.CoreBusiness.Models;
+using de.devcodemonkey.AIChecker.DataSource.APIRequester.Interfaces;
+using de.devcodemonkey.AIChecker.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace de.devcodemonkey.AIChecker.UseCases
+{
+    public class CreatePromptRatingUseCase
+    {
+        private readonly IDefaultMethodesRepository _defaultMethodesRepository;
+        private readonly ILoadUnloadLms _loadUnloadLms;
+        private readonly IAPIRequester _apiRequester;
+
+        public CreatePromptRatingUseCase(IDefaultMethodesRepository defaultMethodesRepository, ILoadUnloadLms loadUnloadLms, IAPIRequester aPIRequester)
+        {
+            _defaultMethodesRepository = defaultMethodesRepository;
+            _loadUnloadLms = loadUnloadLms;
+            _apiRequester = aPIRequester;
+        }
+
+        public async Task ExecuteAsync(string[] modelNames, int maxTokens, string resultSet, Func<string> systemPrompt,
+            Func<string> message, Func<int> ranking, Func<bool> newImprovement, Action<ResponseData> apiResponse)
+        {
+            do
+            {
+                foreach (var modelName in modelNames)
+                {
+                    // change model
+                    _loadUnloadLms.Load(modelName);
+
+                    // send message
+                    List<IMessage> messages = new();
+                    messages.Add(new Message
+                    {
+                        Role = "user",
+                        Content = message()
+                    });
+                    messages.Add(new Message
+                    {
+                        Role = "system",
+                        Content = systemPrompt()
+                    });
+
+                    IApiResult<ResponseData> apiResult = await _apiRequester.SendChatRequestAsync(messages, maxTokens: maxTokens);
+                    // return result to show in UI
+                    apiResponse(apiResult.Data);
+
+                    // save result
+                    var rankingValue = ranking();
+                    var result = new Result
+                    {
+                        ResultId = Guid.NewGuid(),
+                        Message = message(),
+                        Asked = messages[0].Content,
+                        Temperature = 0,
+                        MaxTokens = maxTokens,
+                        PromptTokens = apiResult?.Data?.Usage?.PromptTokens ?? 0,
+                        CompletionTokens = apiResult?.Data?.Usage?.CompletionTokens ?? 0,
+                        TotalTokens = apiResult?.Data?.Usage?.TotalTokens ?? 0,
+                        RequestCreated = DateTimeOffset.FromUnixTimeSeconds(apiResult?.Data?.Created ?? 0).UtcDateTime,
+                        RequestStart = apiResult!.RequestStart,
+                        RequestEnd = apiResult.RequestEnd,
+                        Rating = rankingValue
+                    };
+                    await SaveDependencies.SaveDependenciesFromResult(_defaultMethodesRepository,
+                        systemPrompt(),
+                        resultSet,
+                        apiResult,
+                        result,
+                        apiResult!.Data!.Object!,
+                        apiResult?.Data?.Choices?.FirstOrDefault()?.FinishReason ?? string.Empty
+                        );
+                }
+            }
+            while (newImprovement());
+        }
+    }
+}
--- AIChecker/UseCasesTests/CreatePromptRatingUseCaseTests.cs
+++ AIChecker/UseCasesTests/CreatePromptRatingUseCaseTests.cs
@@ -3,25 +3,30 @@ using de.devcodemonkey.AIChecker.CoreBusiness.DbModels;
 using de.devcodemonkey.AIChecker.DataSource.APIRequester;
 using LmsWrapper;
 using de.devcodemonkey.AIChecker.CoreBusiness.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace de.devcodemonkey.AIChecker.UseCases.Tests
 {
     [TestClass()]
     public class CreatePromptRatingUseCaseTests
     {
+        private readonly DbContextOptions<DbContext> _options = new DbContextOptionsBuilder<DbContext>()
+                .UseInMemoryDatabase(databaseName: "AicheckerTestDatabase")
+                .Options;
+
         [TestMethod()]
         public async Task ExecuteAsyncTest()
         {
             // Arrange
-            CreatePromptRatingUseCase createPromptRatingUseCase = new CreatePromptRatingUseCase(new DefaultMethodesRepository(new AicheckerContext()), new LoadUnloadLms(), new APIRequester());
+            CreatePromptRatingUseCase createPromptRatingUseCase = new CreatePromptRatingUseCase(new DefaultMethodesRepository(new AicheckerContext(_options)), new LoadUnloadLms(), new APIRequester());
 
-            var defaultMethodesRepository = new DefaultMethodesRepository(new AicheckerContext());
-            await defaultMethodesRepository.RecreateDatabaseAsync();
+            var defaultMethodesRepository = new DefaultMethodesRepository(new AicheckerContext(_options));
+            
             var model1 = await defaultMethodesRepository.AddAsync(new Model { ModelId = Guid.NewGuid(), Value = "lmstudio-community/Phi-3.5-mini-instruct-GGUF" });
             var model2 = await defaultMethodesRepository.AddAsync(new Model { ModelId = Guid.NewGuid(), Value = "TheBloke/SauerkrautLM-7B-HerO-GGUF" });
 
             // Act
-            await createPromptRatingUseCase.ExecuteAsync(new string[] { model1.Value, model2.Value }, 300, "resultSet", () => "systemPrompt", () => "message", () => 1, () => true, (ResponseData responseData) => { });
+            await createPromptRatingUseCase.ExecuteAsync(new string[] { model1.Value, model2.Value }, 300, "resultSet", () => "You are helpful assistent", () => "Create me a poem", () => 1, () => false, (ResponseData responseData) => { });
 
         }
     }
