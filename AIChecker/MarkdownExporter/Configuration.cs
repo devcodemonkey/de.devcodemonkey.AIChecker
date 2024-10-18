01c38b5afe1bf975c2fd55e7cf2f0590293da363
David Höll <hoell@hl-dev.de>
2024-10-18 17:01:04 +0200

    feat: Add Configuration and MdCharts class to create charts, the first chart is a bar chart
    Added a new `Configuration` class in `Configuration.cs` to manage
    configuration settings, including a temporary path and a predefined
    set of chart colors.
    
    Updated `MarkdownExporter.csproj` to include a new package reference
    for `ScottPlot` version `5.0.40`.
    
    Introduced a new `MdCharts` class in `MdCharts.cs` for creating bar
    charts using the `ScottPlot` library. This class includes methods to
    generate bar charts and save them as PNG files in a specified
    temporary directory.

---
+++ AIChecker/MarkdownExporter/Configuration.cs
@@ -0,0 +1,48 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace de.devcodemonkey.AIChecker.MarkdownExporter
+{
+    public static class Configuration
+    {
+        public static string TempPath { get; } = Path.Combine(Path.GetTempPath(), "MarkdownExporter");
+
+        public static readonly Color[] ChartColors = new Color[]
+         {
+            Colors.Red,
+            Colors.Blue,
+            Colors.Green,
+            Colors.Orange,
+            Colors.Purple,
+            Colors.Cyan,
+            Colors.Magenta,
+            Colors.Yellow,
+            Colors.Black,
+            Colors.Gray,
+            Colors.DarkBlue,
+            Colors.DarkGreen,
+            Colors.DarkRed,
+            Colors.DarkCyan,
+            Colors.DarkMagenta,
+            Colors.DarkOrange,
+            Colors.Brown,
+            Colors.Pink,
+            Colors.Violet,
+            Colors.Indigo,
+            Colors.Turquoise,
+            Colors.LimeGreen,
+            Colors.Chartreuse,
+            Colors.SkyBlue,
+            Colors.Crimson,
+            Colors.Gold,
+            Colors.Navy,
+            Colors.Teal,
+            Colors.Olive,
+            Colors.Salmon
+         };
+    }
+}
---
+++ AIChecker/MarkdownExporter/MdCharts.cs
@@ -0,0 +1,60 @@
+﻿using de.devcodemonkey.AIChecker.UseCases.PluginInterfaces;
+using ScottPlot;
+
+namespace de.devcodemonkey.AIChecker.MarkdownExporter
+{
+    public class MdCharts
+    {
+        private readonly IMdFile _mdFile;
+
+        public MdCharts()
+        {
+
+        }
+
+        public MdCharts(IMdFile mdFile)
+        {
+            _mdFile = mdFile;
+        }
+
+        public string CreateBarChart(double[] values, string[] descriptions, int width = 400, int heigth = 300)
+        {
+            if (values == null || values.Length == 0)
+                throw new ArgumentException("Values are required.", nameof(values));
+            if (descriptions == null || descriptions.Length == 0)
+                throw new ArgumentException("Descriptions are required.", nameof(descriptions));
+            if (values.Length != descriptions.Length)
+                throw new ArgumentException("Values and descriptions must have the same length.");
+
+            ScottPlot.Plot plot = new();
+
+            var barList = new List<Bar>();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                barList.Add(new Bar
+                {
+                    Position = i,
+                    Value = values[i],
+                    FillColor = Configuration.ChartColors[i],
+                });
+            }
+
+            var bar = plot.Add.Bars(barList.ToArray());
+
+            Tick[] ticks = descriptions.Select((description, index) => new Tick(index, description)).ToArray();
+
+            plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(ticks);
+
+
+            if (!Path.Exists(Configuration.TempPath))
+                Directory.CreateDirectory(Configuration.TempPath);
+
+            var filePath = Path.Combine(Configuration.TempPath, $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.png");
+
+            plot.SavePng(filePath, width, heigth);
+
+            return filePath;
+        }
+    }
+}
