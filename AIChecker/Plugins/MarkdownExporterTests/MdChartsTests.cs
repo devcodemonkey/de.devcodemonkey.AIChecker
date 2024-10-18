3e605abf49e2798faec3f7f3fea0ca98619afd6a
David Höll <hoell@hl-dev.de>
2024-10-18 17:02:56 +0200

    refactor: MdChartsTests: remove usings, add namespace/class
    Removed unnecessary using directives and redefined the namespace
    to include a class `MdChartsTests` with a `TestClass` attribute
    and a private field `_chartService` of type `MdCharts`.

--- AIChecker/Plugins/MarkdownExporterTests/MdChartsTests.cs
+++ AIChecker/Plugins/MarkdownExporterTests/MdChartsTests.cs
@@ -1,13 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using de.devcodemonkey.AIChecker.MarkdownExporter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
-
-namespace de.devcodemonkey.AIChecker.MarkdownExporter.Tests
+﻿namespace de.devcodemonkey.AIChecker.MarkdownExporter.Tests
 {
     [TestClass()]
     public class MdChartsTests
