04441bc1dac391b6ef4a6cc31022f7605dee1b2a
David Höll <hoell@hl-dev.de>
2024-11-17 17:10:29 +0100

    feat: Add JSON format validation use case and validator
    Introduce `CheckJsonFormatOfResultsUseCase` in `de.devcodemonkey.AIChecker.UseCases` to validate JSON format of result messages using `JsonValidator`. The use case fetches results via `IDefaultMethodesRepository` and sets the `IsJson` property based on validation. Add `JsonValidator` in `de.devcodemonkey.AIChecker.UseCases.Global` with `IsValidJson` method to check JSON validity using `System.Text.Json.JsonDocument.Parse`.

---
+++ AIChecker/UseCases/CheckJsonFormatOfResultsUseCase.cs
@@ -0,0 +1,33 @@
+﻿using de.devcodemonkey.AIChecker.CoreBusiness.DbModels;
+using de.devcodemonkey.AIChecker.UseCases.Global;
+using de.devcodemonkey.AIChecker.UseCases.PluginInterfaces;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace de.devcodemonkey.AIChecker.UseCases
+{
+    public class CheckJsonFormatOfResultsUseCase
+    {
+        private readonly IDefaultMethodesRepository _defaultMethodesRepository;        
+
+        public CheckJsonFormatOfResultsUseCase(IDefaultMethodesRepository defaultMethodesRepository)
+            => _defaultMethodesRepository = defaultMethodesRepository;
+
+        public async Task<IEnumerable<Result>> ExecuteAsync(string? ResultSet)
+        {
+            IEnumerable<Result> results;
+            if (ResultSet == null)
+                results = await _defaultMethodesRepository.GetAllEntitiesAsync<Result>();
+            else
+            {
+                var resultSetId = await _defaultMethodesRepository.GetResultSetIdByValueAsync(ResultSet);
+                results = await _defaultMethodesRepository.ViewResultsOfResultSetAsync(resultSetId);
+            }
+
+            foreach (var result in results)                        
+                result.IsJson = JsonValidator.IsValidJson(result.Message);
+
+            return results;
+        }
+    }
+}
---
+++ AIChecker/UseCases/Global/JsonValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace de.devcodemonkey.AIChecker.UseCases.Global
+{
+    public class JsonValidator
+    {
+        public static bool IsValidJson(string? json)
+        {
+            if (json == null)
+                return false;
+            try
+            {
+                JsonDocument.Parse(json);
+                return true;
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+        }
+    }
+}
