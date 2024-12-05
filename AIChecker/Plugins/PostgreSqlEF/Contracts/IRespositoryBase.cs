275a93cd438577d775bd01aad7419959ccd5a4dc
David Höll <hoell@hl-dev.de>
2024-12-05 22:05:36 +0100

    feat: Add repository pattern with CRUD operations
    Introduce IRespositoryBase<T> interface defining CRUD and query methods, both synchronous and asynchronous. Implement RepositoryBase<T> abstract class using Entity Framework Core to provide concrete method implementations.

---
+++ AIChecker/Plugins/PostgreSqlEF/Contracts/IRespositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace de.devcodemonkey.AIChecker.DataStore.PostgreSqlEF.Contracts;
+
+public interface IRespositoryBase<T>
+{    
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+
+    IQueryable<T> GetAll();
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+
+    Task CreateAsync(T entity);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression);
+}
---
+++ AIChecker/Plugins/PostgreSqlEF/Respository/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using de.devcodemonkey.AIChecker.DataStore.PostgreSqlEF.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace de.devcodemonkey.AIChecker.DataStore.PostgreSqlEF.Respository;
+
+public abstract class RepositoryBase<T>(AicheckerContext ctx) : IRespositoryBase<T> where T : class
+{
+    public void Create(T entity) => ctx.Set<T>().Add(entity);
+    public void Delete(T entity) => ctx.Set<T>().Remove(entity);
+    public void Update(T entity) => ctx.Set<T>().Update(entity);
+
+    public IQueryable<T> GetAll() => ctx.Set<T>();
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression) => ctx.Set<T>().Where(expression);
+
+
+    public async Task CreateAsync(T entity) => await ctx.Set<T>().AddAsync(entity);
+
+    public async Task<IEnumerable<T>> GetAllAsync() => await ctx.Set<T>().ToListAsync();
+    public async Task<IEnumerable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression) 
+        => await ctx.Set<T>().Where(expression).ToListAsync();
+}
